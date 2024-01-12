af51929c9f149406279ee3c8d8572ad87ed1f2c4
PGV-da <vikramji283@gmail.com>
2024-01-12 20:23:16 +0530

    schema

--- app/main.py
+++ app/main.py
@@ -3,22 +3,18 @@ import time
 from psycopg2.extras import RealDictCursor
 from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
-from pydantic import BaseModel
+
 from typing import Optional
 from random import randrange
 from sqlalchemy.orm import Session
 
-from . import models
+from . import models, schemas
 from .database import engine, get_db
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = True
 while True:
     try:
         conn = psycopg2.connect(host="localhost", database="fastapi", user="postgres", password="vikram", cursor_factory=RealDictCursor)
@@ -68,7 +64,7 @@ def get_post(id: int, db: Session = Depends(get_db)):
     return {"post details" : post}
 
 @app.post("/posts")
-def create_posts(post: Post, db: Session = Depends(get_db)):
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
     # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """, (post.title, post.content, post.published))
     # new_post = cursor.fetchone()
     # conn.commit()
@@ -92,12 +88,12 @@ def delete_post(id: int, db: Session = Depends(get_db)):
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.put("/posts/{id}")
-def update_post(id: int, updated_post: Post, db: Session = Depends(get_db)):
+def update_post(id: int, updated_post: schemas.Post, db: Session = Depends(get_db)):
     # cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING * """, (post.title, post.content, post.published, str(id)))
     # updated_post = cursor.fetchone()
     # conn.commit()
 
-    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post_query = db.query(models.PostCreate).filter(models.Post.id == id)
     post = post_query.first()
 
     if post == None:
