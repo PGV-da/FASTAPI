265fb3643403c63e5d18d9ba0e344c55acc05430
PGV-da <vikramji283@gmail.com>
2024-01-14 12:29:09 +0530

    hash the password

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ import time
 from psycopg2.extras import RealDictCursor
 from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
-
+from passlib.context import CryptContext
 from typing import Optional, List
 from random import randrange
 from sqlalchemy.orm import Session
@@ -11,6 +11,7 @@ from sqlalchemy.orm import Session
 from . import models, schemas
 from .database import engine, get_db
 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
@@ -107,6 +108,11 @@ def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends
 
 @app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+
+    #hash the password - user.password
+    hashed_password = pwd_context.hash(user.password)
+    user.password = hashed_password
+
     new_user = models.User(**user.dict()) #unpacking the dict
     db.add(new_user)
     db.commit()
