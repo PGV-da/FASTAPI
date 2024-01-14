268917ae0626fc3ccca542d922d7e9154bef792d
PGV-da <vikramji283@gmail.com>
2024-01-15 03:59:06 +0530

    OAuth2

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ from sqlalchemy.orm import Session
 
 from . import models, schemas, utils
 from .database import engine, get_db
-from .routes import post, user
+from .routers import post, user, auth
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -43,6 +43,7 @@ def find_index_post(id):
 
 app.include_router(post.router)
 app.include_router(user.router)
+app.include_router(auth.router)
 
 @app.get("/")
 def root():
